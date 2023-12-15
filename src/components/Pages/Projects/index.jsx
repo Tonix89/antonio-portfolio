89e42c7ea04fdf9a790370ecab8494b2f8cdf056
Antonio Arabejo <antonioarabejo@yahoo.com>
2023-12-15 21:33:44 +0100

    Added modal to display project image.

--- src/components/Pages/Projects/index.jsx
+++ src/components/Pages/Projects/index.jsx
@@ -7,15 +7,18 @@ import {
   CardMedia,
   CardContent,
   CardActions,
+  CardActionArea,
   IconButton,
   Link,
   Avatar,
+  Dialog,
 } from '@mui/material';
 import Auction from '../../../Images/auction.png';
 import Astore from '../../../Images/astore.png';
 import Holidaze from '../../../Images/holidaze.png';
 import Web from '../../../Images/web.png';
 import Github from '../../../Images/github.png';
+import { useState } from 'react';
 
 const projects = [
   {
@@ -47,86 +50,107 @@ const projects = [
 ];
 
 export default function ProjectPage() {
+  const [modal, setModal] = useState(false);
+  const [modalImg, setModalImg] = useState();
+
+  const handleModal = (img) => {
+    setModalImg(img);
+    setModal(true);
+  };
+
+  const handleClose = () => {
+    setModal(false);
+  };
+
   return (
-    <Box sx={{ py: 2 }}>
-      <Container maxWidth="lg" sx={{ minHeight: '81.6vh' }}>
-        <Grid container rowSpacing={2} columnSpacing={2}>
-          {projects.map((project) => {
-            return (
-              <Grid item xs={12} md={6} lg={4} key={project.id}>
-                <Card
-                  sx={{
-                    maxWidth: '100%',
-                    height: '550px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-between',
-                  }}
-                >
-                  <Box>
-                    <CardMedia
-                      sx={{ height: 280 }}
-                      image={project.image}
-                      title={project.title}
-                    />
-                    <CardContent>
-                      <Typography
-                        variant="h5"
-                        sx={{ color: 'primary.main', textAlign: 'center' }}
-                      >
-                        {project.title}
-                      </Typography>
-                      <Typography variant="body1" sx={{ textIndent: '20px' }}>
-                        {project.description}
-                      </Typography>
-                    </CardContent>
-                  </Box>
-                  <CardActions
+    <>
+      <Box sx={{ py: 2 }}>
+        <Container maxWidth="lg" sx={{ minHeight: '81.6vh' }}>
+          <Grid container rowSpacing={2} columnSpacing={2}>
+            {projects.map((project) => {
+              return (
+                <Grid item xs={12} md={6} lg={4} key={project.id}>
+                  <Card
                     sx={{
+                      maxWidth: '100%',
+                      height: '550px',
                       display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
+                      flexDirection: 'column',
+                      justifyContent: 'space-between',
                     }}
                   >
-                    <IconButton>
-                      <Link
-                        href={project.repo}
-                        sx={{ color: 'secondary.main' }}
+                    <Box>
+                      <CardActionArea
+                        onClick={() => handleModal(project.image)}
                       >
-                        <Avatar
-                          variant="rounded"
-                          alt={project.title}
-                          src={Github}
-                          sx={{
-                            width: { xs: 30, sm: 48 },
-                            height: { xs: 30, sm: 48 },
-                          }}
+                        <CardMedia
+                          sx={{ height: 280 }}
+                          image={project.image}
+                          title={project.title}
                         />
-                      </Link>
-                    </IconButton>
-                    <IconButton>
-                      <Link
-                        href={project.live_site}
-                        sx={{ color: 'secondary.main' }}
-                      >
-                        <Avatar
-                          variant="rounded"
-                          alt="Website Logo"
-                          src={Web}
-                          sx={{
-                            width: { xs: 30, sm: 48 },
-                            height: { xs: 30, sm: 48 },
-                          }}
-                        />
-                      </Link>
-                    </IconButton>
-                  </CardActions>
-                </Card>
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Container>
-    </Box>
+                      </CardActionArea>
+                      <CardContent>
+                        <Typography
+                          variant="h5"
+                          sx={{ color: 'primary.main', textAlign: 'center' }}
+                        >
+                          {project.title}
+                        </Typography>
+                        <Typography variant="body1" sx={{ textIndent: '20px' }}>
+                          {project.description}
+                        </Typography>
+                      </CardContent>
+                    </Box>
+                    <CardActions
+                      sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <IconButton>
+                        <Link
+                          href={project.repo}
+                          sx={{ color: 'secondary.main' }}
+                        >
+                          <Avatar
+                            variant="rounded"
+                            alt={project.title}
+                            src={Github}
+                            sx={{
+                              width: { xs: 30, sm: 48 },
+                              height: { xs: 30, sm: 48 },
+                            }}
+                          />
+                        </Link>
+                      </IconButton>
+                      <IconButton>
+                        <Link
+                          href={project.live_site}
+                          sx={{ color: 'secondary.main' }}
+                        >
+                          <Avatar
+                            variant="rounded"
+                            alt="Website Logo"
+                            src={Web}
+                            sx={{
+                              width: { xs: 30, sm: 48 },
+                              height: { xs: 30, sm: 48 },
+                            }}
+                          />
+                        </Link>
+                      </IconButton>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Container>
+      </Box>
+      <Dialog open={modal} onClose={handleClose}>
+        <Box component="img" src={modalImg} />
+      </Dialog>
+    </>
   );
 }
