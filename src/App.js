a00810517f1430d57740b138027ac2dae653e279
Antonio Arabejo <antonioarabejo@yahoo.com>
2023-12-16 08:30:12 +0100

    Added personal information in about me page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/Pages/Home";
 import ProjectPage from "./components/Pages/Projects";
 import Layout from "./components/Layout/Layout";
+import AboutMePage from "./components/Pages/About";
 import { Box } from "@mui/material";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="/projects" element={<ProjectPage />} />
+            <Route path="/about" element={<AboutMePage />} />
           </Route>
         </Routes>
     </Box>
